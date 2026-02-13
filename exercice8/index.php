2893ee52a4ec6150b45a7f841592738afd04d27b
auroregql <aurore.gicquel.06@gmail.com>
2026-02-13 15:28:14 +0000

    Exercice 8 terminé : Le Formulaire d'Email (POST)

---
+++ exercice8/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulaire email</title>
+</head>
+<body>
+
+<h1>Entrer votre email</h1>
+
+<form action="traitement_email.php" method="post">
+    <label for="email">Email :</label>
+    <input type="email" name="email" id="email">
+
+    <button type="submit">Envoyer</button>
+</form>
+
+</body>
+</html>
---
+++ exercice8/traitement_email.php
@@ -0,0 +1,20 @@
+<?php
+$email = $_POST["email"];
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Résultat</title>
+</head>
+<body>
+
+<h1>Résultat</h1>
+
+<p>Votre adresse email est : <?= $email ?></p>
+
+<a href="index.php">Retour</a>
+
+</body>
+</html>
