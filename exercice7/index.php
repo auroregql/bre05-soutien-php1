7f3ccd7b2de2b6f03b0be80e2a8282025008c33a
auroregql <aurore.gicquel.06@gmail.com>
2026-02-13 15:02:18 +0000

    Exercice 7 terminé : Exercice 7 : Le Formulaire de Nom (GET)﻿

---
+++ exercice7/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulaire</title>
+</head>
+<body>
+
+<h1>Entrer votre nom</h1>
+
+<form action="traitement_nom.php" method="get">
+    <label for="nom">Nom :</label>
+    <input type="text" name="nom" id="nom">
+
+    <button type="submit">Envoyer</button>
+</form>
+
+</body>
+</html>
---
+++ exercice7/traitement_nom.php
@@ -0,0 +1,16 @@
+<?php
+$nom = $_GET["nom"];
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Bienvenue</title>
+</head>
+<body>
+
+<h1>Bonjour, <?= $nom ?> !</h1>
+
+</body>
+</html>
